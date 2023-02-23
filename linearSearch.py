0977583f711142899a4cd4daa382693c932e123e
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-22 22:20:07 -0600

    Improving linear search algorithm just to make it more dinamic

--- linearSearch.py
+++ linearSearch.py
@@ -1,3 +1,5 @@
+import random
+
 def linearSearch(items, target):
 	result = False
 
@@ -9,4 +11,13 @@ def linearSearch(items, target):
 	return result
 
 
-print(linearSearch(["irv", "ces", "dieg", "mom", "dad", "aunt", "cousins", "irv"], "irv"))
+if __name__ == "__main__":
+	size = int( input("What's the size of the list ") )
+	target = input("What number are you looking for? ")
+	items = [random.randint(0, 100) for i in range(size)]
+
+	res = linearSearch(items, target)
+	resultText = "is" if res else "is not"
+
+	print(items)
+	print(f"The target {target} {resultText} in the list")
